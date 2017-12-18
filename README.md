6b397888a79df119b4fdfce214c198cd19e42b88
gretacb <carol@mapbox.com>
2017-12-18 15:22:47 -0600

    Use vtzero decoding for simple validation (phase 1)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # vtvalidate
 
-[![badge](https://mapbox.s3.amazonaws.com/cpp-assets/node-cpp-skel-badge_blue.svg)](https://github.com/mapbox/node-cpp-skel)  [![Build Status](https://travis-ci.org/mapbox/vtvalidate.svg?branch=master)](https://travis-ci.org/mapbox/vtvalidate)
+[![badge](https://mapbox.s3.amazonaws.com/cpp-assets/node-cpp-skel-badge_blue.svg)](https://github.com/mapbox/node-cpp-skel)  [![Build Status](https://travis-ci.org/mapbox/vtvalidate.svg?branch=master)](https://travis-ci.org/mapbox/vtvalidate) [![codecov](https://codecov.io/gh/mapbox/vtvalidate/branch/master/graph/badge.svg)](https://codecov.io/gh/mapbox/vtvalidate)
 
 
--- test/isValid.test.js
+++ test/isValid.test.js
@@ -1,44 +1,191 @@
 var test = require('tape');
 var module = require('../lib/index.js');
+var mvtf = require('@mapbox/mvt-fixtures');
 
-test('success: prints loud busy world', function(t) {
-  module.isValid({ louder: true }, function(err, result) {
+test('success: valid tile', function(t) {
+  var buffer = mvtf.get('043').buffer;
+  module.isValid(buffer, function(err, result) {
     if (err) throw err;
-    t.equal(result, '...threads are busy async bees...hello world!!!!');
+    t.equal(result, '');
     t.end();
   });
 });
 
-test('success: prints regular busy world', function(t) {
-  module.isValid({ louder: false }, function(err, result) {
-    if (err) throw err;
-    t.equal(result, '...threads are busy async bees...hello world');
+test('failure: invalid arg type', function(t) {
+  module.isValid('woops', function(err, result) {
+    t.ok(err)
+    t.equal(err.message, 'first arg \"buffer\" must be a Protobuf object');
     t.end();
   });
 });
 
-test('error: handles invalid louder value', function(t) {
-  module.isValid({ louder: 'oops' }, function(err, result) {
-    t.ok(err, 'expected error');
-    t.ok(err.message.indexOf('option \'louder\' must be a boolean') > -1, 'expected error message');
+test('failure: invalid arg type', function(t) {
+  module.isValid(null, function(err, result) {
+    t.ok(err)
+    t.equal(err.message, 'first arg is empty');
     t.end();
   });
 });
 
-test('error: handles invalid options value', function(t) {
-  module.isValid('oops', function(err, result) {
-    t.ok(err, 'expected error');
-    t.ok(err.message.indexOf('first arg \'options\' must be an object') > -1, 'expected error message');
+test('failure: invalid arg type', function(t) {
+  module.isValid(undefined, function(err, result) {
+    t.ok(err)
+    t.equal(err.message, 'first arg is empty');
     t.end();
   });
 });
 
-test('error: handles missing callback', function(t) {
+test('failure: missing callback', function(t) {
+  var buffer = mvtf.get('043').buffer;
   try {
-    module.isValid({ louder: 'oops' }, {});
-  } catch (err) {
-    t.ok(err, 'expected error');
-    t.ok(err.message.indexOf('second arg \'callback\' must be a function') > -1, 'expected error message');
+    module.isValid(buffer);
+  } catch(err) {
+    t.ok(err)
+    t.equal(err.message, 'second arg \"callback\" must be a function');
     t.end();
   }
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('003').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'unknown geometry type');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('004').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'Missing geometry field in feature (spec 4.2)');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('005').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'unpaired property key/value indexes (spec 4.4)');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('006').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'Unknown geometry type (spec 4.3.4)');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('007').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'unknown field in layer (tag=15, type=2)');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('008').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'unknown field in layer (tag=5, type=2)');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('011').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'index out of range: 1');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('012').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'unknown vector tile version: 99');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('013').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'unknown field in layer (tag=3, type=0)');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('014').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'missing name field in layer (spec 4.1)');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('023').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'missing name field in layer (spec 4.1)');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('030').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'Feature has more than one geometry field');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('040').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'index out of range: 2');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('041').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'index out of range: 106');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('042').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'index out of range: 2');
+    t.end();
+  });
+});
+
+test('success: invalid tile', function(t) {
+  var buffer = mvtf.get('044').buffer;
+  module.isValid(buffer, function(err, result) {
+    if (err) throw err;
+    t.equal(result, 'expected command 1 but got 7');
+    t.end();
+  });
 });
