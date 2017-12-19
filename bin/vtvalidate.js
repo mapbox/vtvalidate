85ae97e7082733247beaf4afc22ecd82b530b729
Dane Springmeyer <dane@mapbox.com>
2017-12-18 17:45:41 -0800

    add cli program to test arbitrary tiles on the command line

---
+++ bin/vtvalidate.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var exists = require('fs').existsSync;
+var fs = require('fs');
+var zlib = require('zlib');
+
+var usage = 'usage:';
+usage += '\n  vtvalidate.js <vector tile>';
+
+var input_tile = process.argv[2];
+if (!input_tile) {
+   console.log(usage);
+   process.exit(1);
+}
+
+if (!exists(input_tile)) {
+    console.log(input_tile + ' does not exist');
+    process.exit(1);
+}
+
+var validator = require('../');
+
+var buffer = fs.readFileSync(input_tile);
+
+if (buffer[0] === 0x1F && buffer[1] === 0x8B) {
+    console.log('detected gzip compressed tile');
+    var uncompressed_buffer = zlib.gunzipSync(buffer);
+    validator.isValid(buffer,function(err,valid) {
+        console.log(err,valid);
+    });    
+} else {
+    validator.isValid(buffer,function(err,valid) {
+        console.log(err,valid);
+    });    
+}
+
