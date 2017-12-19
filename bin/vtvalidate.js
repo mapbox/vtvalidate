97f6a39ee0a892f95506893a98729432ce5c8676
Dane Springmeyer <dane@mapbox.com>
2017-12-18 17:56:11 -0800

    fix variable name

--- bin/vtvalidate.js
+++ bin/vtvalidate.js
@@ -27,7 +27,7 @@ var buffer = fs.readFileSync(input_tile);
 if (buffer[0] === 0x1F && buffer[1] === 0x8B) {
     console.log('detected gzip compressed tile');
     var uncompressed_buffer = zlib.gunzipSync(buffer);
-    validator.isValid(buffer,function(err,valid) {
+    validator.isValid(uncompressed_buffer,function(err,valid) {
         console.log(err,valid);
     });    
 } else {
