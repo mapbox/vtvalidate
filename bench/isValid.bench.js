6a48be51cb5e27e2ff8c995fff05b6f14e0d3010
Dane Springmeyer <dane@mapbox.com>
2018-06-16 07:27:45 -0700

    modify benchmark to represent mapbox-tile-copy before/after scenario

--- bench/isValid.bench.js
+++ bench/isValid.bench.js
@@ -5,6 +5,7 @@ if (!argv.iterations || !argv.concurrency) {
   console.error('Please provide desired iterations, concurrency');
   console.error('Example: \n\tnode bench/isValid.bench.js --iterations 50 --concurrency 10');
   console.error('Optional args: \n\t--mem (reports memory stats)');
+  console.error('Optional args: \n\t--gzipjs (if passed the decompression is done in JS before passing to vtvalidate. If not passed the decompression is done internally in the threadpool)');
   process.exit(1);
 }
 
@@ -20,7 +21,8 @@ var assert = require('assert')
 var d3_queue = require('d3-queue');
 var module = require('../lib/index.js');
 var queue = d3_queue.queue();
-var bytes = require('bytes');  
+var bytes = require('bytes');
+var zlib = require('zlib');
 
 var p = 'node_modules/@mapbox/mvt-fixtures/real-world/chicago/';
 
@@ -31,7 +33,7 @@ fs.readdir(p, function (err, files) {
 });
 
 var start = function(files){
-  var track_mem = argv.mem ? true : false; 
+  var track_mem = argv.mem ? true : false;
   var iterations = argv.iterations;
   var concurrency = argv.concurrency;
   var runs = 0;
@@ -41,41 +43,61 @@ var start = function(files){
     max_heap:0,
     max_heap_total:0
   };
-    
+
   files.forEach(function(file) {
     var path = p+file;
     var buffer = fs.readFileSync(path);
 
-    tiles.push(buffer);
-  });  
+    // compress tiles such that the
+    // benchmark has to decompress on the fly
+    tiles.push(zlib.gzipSync(buffer));
+  });
 
   function run(buffer, cb) {
-    module.isValid(buffer, function(err, result) {
-        if (err) {
-          return cb(err);
-        }
-        ++runs;
-        if (track_mem && runs % 1000) {
-          var mem = process.memoryUsage();
-          if (mem.rss > memstats.max_rss) memstats.max_rss = mem.rss;
-          if (mem.heapTotal > memstats.max_heap_total) memstats.max_heap_total = mem.heapTotal;
-          if (mem.heapUsed > memstats.max_heap) memstats.max_heap = mem.heapUsed;
-        }
-        return cb();
-    });
+    if (argv.gzipjs) {
+      zlib.gunzip(buffer,function(err,uncompressed) {
+          module.isValid(uncompressed, function(err, result) {
+              if (err) {
+                return cb(err);
+              }
+              ++runs;
+              if (track_mem && runs % 1000) {
+                var mem = process.memoryUsage();
+                if (mem.rss > memstats.max_rss) memstats.max_rss = mem.rss;
+                if (mem.heapTotal > memstats.max_heap_total) memstats.max_heap_total = mem.heapTotal;
+                if (mem.heapUsed > memstats.max_heap) memstats.max_heap = mem.heapUsed;
+              }
+              return cb();
+          });
+      });
+    } else {
+      module.isValid(buffer, function(err, result) {
+          if (err) {
+            return cb(err);
+          }
+          ++runs;
+          if (track_mem && runs % 1000) {
+            var mem = process.memoryUsage();
+            if (mem.rss > memstats.max_rss) memstats.max_rss = mem.rss;
+            if (mem.heapTotal > memstats.max_heap_total) memstats.max_heap_total = mem.heapTotal;
+            if (mem.heapUsed > memstats.max_heap) memstats.max_heap = mem.heapUsed;
+          }
+          return cb();
+      });
+    }
   }
 
   // Start monitoring time before async work begins within the defer iterator below.
-  // AsyncWorkers will kick off actual work before the defer iterator is finished, 
+  // AsyncWorkers will kick off actual work before the defer iterator is finished,
   // and we want to make sure we capture the time of the work of that initial cycle.
-  console.log('Running benchmark...'); 
-  var time = +(new Date());  
+  console.log('Running benchmark...');
+  var time = +(new Date());
 
   for (var i = 0; i < iterations; i++) {
     tiles.forEach(function(tile) {
       queue.defer(run, tile);
     });
-  }  
+  }
 
   queue.awaitAll(function(error) {
     if (error) throw error;
@@ -83,7 +105,7 @@ var start = function(files){
       throw new Error('Error: did not run as expected');
     }
     // check rate
-    time = +(new Date()) - time;  
+    time = +(new Date()) - time;
 
     if (time == 0) {
       console.log('Warning: ms timer not high enough resolution to reliably track rate. Try more iterations');
@@ -97,12 +119,12 @@ var start = function(files){
       } else {
         console.log('Note: pass --mem to track memory usage');
       }
-    }  
+    }
 
     console.log('Benchmark iterations: ', iterations, 'concurrency: ', concurrency);
 
     // There may be instances when you want to assert some performance metric
-    //assert.equal(rate > 1000, true, 'speed not at least 1000/second ( rate was ' + rate + ' runs/s )');  
+    //assert.equal(rate > 1000, true, 'speed not at least 1000/second ( rate was ' + rate + ' runs/s )');
 
   });
 
